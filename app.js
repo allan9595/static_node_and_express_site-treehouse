b29d76dccc45130b5c4747a4e5789ab2e2b448d1
*allan9595* <bohanzhang@my.unt.edu>
2019-07-16 17:40:50 -0400

    add more images, add error detection in express middleware

--- app.js
+++ app.js
@@ -4,32 +4,41 @@ const path = require("path");
 const app = express();
 const port = 3000; 
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug"); //use pug
+app.set("views", path.join(__dirname, "views")); //hook the pug up with express
 
 app.use('/static', express.static(path.join(__dirname, "public"))) //use express static to serve up static file in public
 app.use('/images', express.static(path.join(__dirname, 'images'))) //serve up images as static files
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { //render index page and pass data to pug template
     res.render("index", {
-       data
+       data 
     })
 });
 
 app.get("/about", (req, res) => {
-    res.render("about")
+    res.render("about") //render about page with pug
 })
 
 app.get("/project/:id", (req, res) => {
+    //render project page with pug and use dynamic query id
     res.render('project', {
         data,
         data: data[req.params.id]
     })
 })
 
-app.use((err, req, res, next) => {
-    console.log(err)
+app.use((req, res, next) => {
+    const err = new Error('Not found');
+    err.status = 404; //handle 404 
+    next(err);
 })
+
+app.use((err, req, res, next) => {
+    res.status(err.status);
+    res.render('error', {error: err}); //render error in error template
+})
+
 
 app.listen(port, () => {
     console.log(`App is running on ${port} !!!`)
